3617667ea2200c237e0439e5197dde51167a83ca
JERIN RAJ <jerin@notavai.local>
2026-04-13 09:55:24 +0530

    Updated for running in Server

--- mechanical-stress/vite.config.js
+++ mechanical-stress/vite.config.js
@@ -1,9 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   preview: {
+//     allowedHosts: ['mechanical-stress-analyzer-kits.up.railway.app'] // Replace with your actual Railway URL
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   preview: {
-    allowedHosts: ['mechanical-stress-analyzer-kits.up.railway.app'] // Replace with your actual Railway URL
+    host: '0.0.0.0',
+    // Railway provides the PORT variable automatically
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    strictPort: true, 
+    allowedHosts: true 
   }
 })
